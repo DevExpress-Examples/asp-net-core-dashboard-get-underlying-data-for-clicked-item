ad9173301fb923e7d379619f6192b6a269f02ca6
Polina Tyureva <polina.tyureva@devexpress.com>
2022-10-20 18:49:34 +0400

    update cs solution

---
+++ CS/AspNetCoreDashboard_UnderlyingData/Code/DashboardUtils.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardCommon;
+using DevExpress.DashboardWeb;
+using DevExpress.DataAccess.Excel;
+using DevExpress.DataAccess.Sql;
+using Microsoft.Extensions.FileProviders;
+
+namespace AspNetCoreDashboard_UnderlyingData {
+    public static class DashboardUtils {
+        public static DashboardConfigurator CreateDashboardConfigurator(IConfiguration configuration, IFileProvider fileProvider) {
+            DashboardConfigurator configurator = new DashboardConfigurator();
+            configurator.SetConnectionStringsProvider(new DashboardConnectionStringsProvider(configuration));
+
+            DashboardFileStorage dashboardFileStorage = new DashboardFileStorage(fileProvider.GetFileInfo("Data/Dashboards").PhysicalPath);
+            configurator.SetDashboardStorage(dashboardFileStorage);
+
+            DataSourceInMemoryStorage dataSourceStorage = new DataSourceInMemoryStorage();
+
+            configurator.ConfigureDataConnection += (s, e) => {
+                e.ConnectionParameters = new ExtractDataSourceConnectionParameters(fileProvider.GetFileInfo("Data/SalesPerson.dat").PhysicalPath);
+            };
+            return configurator;
+        }
+    }
+}
